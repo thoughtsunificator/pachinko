324fe85127d559ccef1bdc70c0c92dd22d5c781f
Romain Lebesle <oss@thoughtsunificator.me>
2021-12-26 17:26:41 +0100

    fixed pagination

--- src/View/Partial/paginate.php
+++ src/View/Partial/paginate.php
@@ -3,8 +3,11 @@
 		<a href="?<?= http_build_query(array_merge( $_GET, array( 'page' => (isset($page) && is_numeric($page) ?  ($page - 1) : 1)))); ?>">Precedent</a>
 	<?php endif;?>
 	<?php
-	for ($i=($page >= $config::$RESULTS_PER_PAGE ? (($page + $config::$RESULTS_PER_PAGE) <= ($totalPage) ? ($page - 8) : ($totalPage - $config::$RESULTS_PER_PAGE)) : 0); $i < ($page >= $config::$RESULTS_PER_PAGE ? (($page + $config::$RESULTS_PER_PAGE) <= ($totalPage) ? ($page + 8) : $totalPage) : ($totalPage < $config::$RESULTS_PER_PAGE ? $totalPage : $config::$RESULTS_PER_PAGE)); $i++):?>
-		<a class="text-decoration-none padding-vertical-small padding-horizontal <?= (($i + 1) === $page) ? "background-color-zambezi" : "background-color-mineshaft" ?> border-width-1 border-style-solid border-color-dustygray2 color-bonjour" href="?<?= http_build_query(array_merge( $_GET, array( 'page' => $i + 1))); ?>"><?= $i + 1 ?></a>
+	$startPageIndex = max(0, $page - round($config::$PAGES_SHOWN / 2));
+	$left = $config::$PAGES_SHOWN - abs($page - ($startPageIndex));
+	$endPageIndex = min($totalPage, $page + $left);
+	for ($i = $startPageIndex; $i < $endPageIndex; $i++):?>
+		<a class="<?= (($i + 1) == $page) ? "active" : "" ?>" href="?<?= http_build_query(array_merge( $_GET, array( 'page' => $i + 1))); ?>"><?= $i + 1 ?></a>
 	<?php endfor; ?>
 	<?php if (isset($page) && is_numeric($page) && $page < $totalPage): ?>
 		<a href="?<?= http_build_query(array_merge( $_GET, array( 'page' => (isset($page) && is_numeric($page) ?  ($page + 1) : 2)))); ?>">Suivant</a>
