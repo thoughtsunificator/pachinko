25f8a8be616e73aef58d25a377c7229f1c251e2e
Romain Lebesle <oss@thoughtsunificator.me>
2021-12-26 21:06:30 +0100

    fix

--- src/View/Partial/paginate.php
+++ src/View/Partial/paginate.php
@@ -6,9 +6,16 @@
 	$startPageIndex = max(0, $page - round($config::$PAGES_SHOWN / 2));
 	$left = $config::$PAGES_SHOWN - abs($page - ($startPageIndex));
 	$endPageIndex = min($totalPage, $page + $left);
-	for ($i = $startPageIndex; $i < $endPageIndex; $i++):?>
+	?>
+	<?php	if ($startPageIndex > 0): ?>
+		<a href="?<?= http_build_query(array_merge( $_GET, array( 'page' => 1))); ?>">1</a>
+	<?php endif;?>
+	<?php for ($i = $startPageIndex; $i < $endPageIndex; $i++):?>
 		<a class="<?= (($i + 1) == $page) ? "active" : "" ?>" href="?<?= http_build_query(array_merge( $_GET, array( 'page' => $i + 1))); ?>"><?= $i + 1 ?></a>
 	<?php endfor; ?>
+	<?php	if ($endPageIndex < $totalPage): ?>
+		<a href="?<?= http_build_query(array_merge( $_GET, array( 'page' => $totalPage))); ?>"><?= $totalPage ?></a>
+	<?php endif;?>
 	<?php if (isset($page) && is_numeric($page) && $page < $totalPage): ?>
 		<a href="?<?= http_build_query(array_merge( $_GET, array( 'page' => (isset($page) && is_numeric($page) ?  ($page + 1) : 2)))); ?>">Suivant</a>
 	<?php endif;?>
